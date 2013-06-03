83cb2f6ce6d0c2783635bca766cf80c0c65e2e05
Synox <sydub@dubgo.com>
2013-06-03 23:50:13 +0200

    refactoring, added actions

--- main.cpp
+++ main.cpp
@@ -1,45 +1,84 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
+
+
+class Cmd {
+private:
+    vector<string> options;
+    string value;
+    const string OPEN_TAG  = "<";
+    const string CLOSE_TAG  = ">";
+    const string SLASH  = "/";
+public:
+    Cmd(vector<string> options, string value): options(options), value(value) {}
+
+    string toString() {
+        // work from inside to outside: first value, then last option, second last option, and so on.
+
+        string optionString;
+
+        // value
+        optionString.append(value);
+
+        // reverse order!
+        for_each(options.rbegin(), options.rend(), [&](string option){
+            optionString.insert(0, OPEN_TAG + option + CLOSE_TAG);
+            optionString.append(OPEN_TAG +SLASH + option + CLOSE_TAG);
+        });
+
+        optionString.insert(0, R"(<YAMAHA_AV cmd=\"PUT\">)");
+        optionString.append("</YAMAHA_AV>");
+
+        return optionString;
+    }
+};
 
 /**
  * @brief The YamahaControl class provides an API to the yamaha RX-V_71 (and A_10) series AVR.
  */
 class YamahaControl {
 public:
-    YamahaControl(string hostname) : hostname(hostname) {}
-    void runCommand(vector<string> options, string value);
-private:
-    string hostname;
-    const string OPEN_TAG  = "<";
-    const string CLOSE_TAG  = ">";
-    const string SLASH  = "/";
-};
-
-void YamahaControl::runCommand(vector<string> options, string value) {
-    // work from inside to outside: first value, then last option, second last option, and so on.
-
-    string optionString;
-
-    // value
-    optionString.append(value);
-
-    for (vector<string>::reverse_iterator i = options.rbegin(); i != options.rend(); i++) {
-        optionString.insert(0, OPEN_TAG + *i + CLOSE_TAG);
-        optionString.append(OPEN_TAG +SLASH + *i + CLOSE_TAG);
+    YamahaControl(string hostname) {
+        url = string("http://").append(hostname).append("/YamahaRemoteControl/ctrl");
+    }
+    void runCommand(Cmd& cmd);
+    bool runAction(string keyword) {
+        auto iterator= actions.find(keyword);
+        if(iterator!=actions.end()) {
+            pair<string,Cmd> pair = *iterator;
+            Cmd cmd = pair.second;
+            cout << "running command "<<keyword<<endl;
+            runCommand(cmd);
+            return true;
+        } else {
+            return false;
+        }
     }
 
+    void addAction(string keyword, vector<string> options, string value) {
+        Cmd cmd(options,value);
+        map<string, Cmd>::value_type pair(keyword, cmd);
+        actions.insert(pair);
+    }
 
-    optionString.insert(0, R"(<YAMAHA_AV cmd=\"PUT\">)");
-    optionString.append("</YAMAHA_AV>");
+    void loadActions() {
+        addAction("down", {"Main_Zone", "Volume", "Lvl"} ,"<Val>Down 2 dB</Val><Exp></Exp><Unit></Unit>");
+        addAction("up",   {"Main_Zone", "Volume", "Lvl"} ,"<Val>Up 2 dB</Val><Exp></Exp><Unit></Unit>");
+    }
 
-    string url = string("http://").append(this->hostname).append("/YamahaRemoteControl/ctrl");
+private:
+    string url;
+    map<string,Cmd> actions;
+};
 
-    string command;
-    command.append(R"(/usr/bin/curl --data ")" );
-    command.append(optionString);
+void YamahaControl::runCommand(Cmd& cmd) {
+
+    string command = R"(/usr/bin/curl --data ")";
+    command.append(cmd.toString());
     command.append(R"(" )");
     command.append(url);
 
@@ -49,32 +88,26 @@ void YamahaControl::runCommand(vector<string> options, string value) {
 
 
 
+
 int main(int argc, char** argv)
 {
-    if (argc < 3)
+    if (argc != 3)
     {
-        cout << "Usage: " << argv[0] << " <hostname> <option>* <value>" << endl;
+        cout << "Usage: " << argv[0] << " <hostname> <action>" << endl;
         cout << "       sets the value on the yamaha device" << endl;
-        cout << "       example: Main_Zone Volume Lvl \"<Val>Down 2 dB</Val><Exp></Exp><Unit></Unit>\" " << endl;
-        cout << "                Main_Zone 2Volume Mute On/Off " << endl;
-
+        cout << "       example: 192.168.1.45 up" << endl;
         return -1;
     }
 
+
     char* hostname = argv[1];
-    YamahaControl l(hostname);
+    YamahaControl control(hostname);
+    control.loadActions();
 
-    // copy options
-    vector<string> options;
-    for(int i=2; i < argc-1; i++) {
-        options.push_back(argv[i]);
-    }
 
-    // value
-    string value = std::string(argv[argc-1]);
-
-    // run
-    l.runCommand(options, value);
+    // name
+    string action = std::string(argv[2]);
+    control.runAction(action);
 
     return 0;
 }
--- yamaha_avr_api.pro
+++ yamaha_avr_api.pro
@@ -5,7 +5,14 @@ CONFIG -= qt
 
 SOURCES += main.cpp
 
-QMAKE_CXXFLAGS += -std=c++11
+QMAKE_CXXFLAGS += -std=c++0x
+QMAKE_CXXFLAGS = -std=c++11 -stdlib=libc++ -mmacosx-version-min=10.7
+QMAKE_LFLAGS = -std=c++11 -stdlib=libc++ -mmacosx-version-min=10.7
+
+INCLUDEPATH += /Applications/Tools/boost/boost_1_53_0/boost/
 
 unix: CONFIG += link_pkgconfig
 unix: PKGCONFIG +=
+
+OTHER_FILES += \
+    README.md
