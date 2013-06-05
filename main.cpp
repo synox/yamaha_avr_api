5140095957a9028baeca6c71128057b5277da635
Synox <sydub@dubgo.com>
2013-06-05 15:34:34 +0200

    rewrite, removed action concept
    now we have simple calls

--- main.cpp
+++ main.cpp
@@ -5,29 +5,81 @@
 #include <curl/curl.h>
 #include "interactive_mode.hpp"
 #include "actionrunner.hpp"
+#include <ncurses.h>
 
 
 using namespace std;
 
+class Http {
+private:
+    CURL* curl;
+    struct curl_slist* slist;
+    string response;
 
-class Cmd {
-protected:
-    vector<string> options;
-    string value;
-    const string OPEN_TAG  = "<";
-    const string CLOSE_TAG  = ">";
-    const string SLASH  = "/";
-    string method;
+    /**
+     * @brief readHttpResponse reads the http response and writes to the string
+     */
+    size_t readHttpResponse(void *ptr, size_t size, size_t count, string *stream) {
+        stream->append((char*)ptr, 0, size*count);
+        return size*count;
+    }
 public:
-    Cmd(vector<string> options, string value, string method = "PUT")  {
-        this->options=options;
-        this->value=value;
-        this->method=method;
+    Http() {
+        curl = curl_easy_init();
+        slist = curl_slist_append(NULL, "Content-Type: text/xml; charset=utf-8");
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, slist);
+        curl_easy_setopt(curl, CURLOPT_TIMEOUT , 1L);
+        curl_easy_setopt(curl, CURLOPT_VERBOSE, 0);
+
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response); // used in readHttpResponse
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &Http::readHttpResponse);
+
+    }
+    ~Http() {
+        curl_slist_free_all(slist);
+        curl_easy_cleanup(curl);
+    }
+    bool request(string& url, string& xmlString) {
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_POST , 1);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, xmlString.c_str());
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE  , xmlString.length());
+
+        CURLcode res = curl_easy_perform(curl);
+        if(res != CURLE_OK) {
+            response = string("curl_easy_perform() failed: ") +  curl_easy_strerror(res);
+            return false;
+        }
+        return true;
+    }
+    string& getResponse() {return response;}
+
+
+};
+
+
+/**
+ * @brief The YamahaControl class provides an API to the yamaha RX-V_71 (and A_10) series AVR.
+ */
+class YamahaControl {
+private:
+    string url;
+    Http http;
+public:
+    YamahaControl(char* hostname) {
+        url = string("http://").append(hostname).append("/YamahaRemoteControl/ctrl").c_str();
     }
 
-    string toString() {
-        // work from inside to outside: first value, then last option, second last option, and so on.
+    string selectInput(string inputName, int suffix) {
+        inputName.append(std::to_string(suffix));
+        return selectInput(inputName);
+    }
 
+    string selectInput(string inputName) {
+        return run({"Main_Zone", "Input", "Input_Sel"} ,inputName, "PUT");
+    }
+
+    string run(vector<string> options, string value, const char* method) {
         string optionString;
 
         // value
@@ -35,143 +87,100 @@ public:
 
         // reverse order!
         for_each(options.rbegin(), options.rend(), [&](string option){
-            optionString.insert(0, OPEN_TAG + option + CLOSE_TAG);
-            optionString.append(OPEN_TAG +SLASH + option + CLOSE_TAG);
+            optionString.insert(0, "<" + option + ">");
+            optionString.append("</" + option + ">");
         });
 
+        return run(optionString, method);
+    }
+
+    string run(string optionString, const char* method) {
         string prefix = R"(<YAMAHA_AV cmd=")";
         prefix.append(method);
         prefix.append(R"(">)");
 
         optionString.insert(0, prefix);
         optionString.append("</YAMAHA_AV>");
-
-        return optionString;
+        http.request(url, optionString);
+        return http.getResponse();
     }
 };
 
 
-/**
- * @brief The YamahaControl class provides an API to the yamaha RX-V_71 (and A_10) series AVR.
- */
-class YamahaControl: public ActionRunner {
-public:
-    YamahaControl(string hostname) {
-        url = string("http://").append(hostname).append("/YamahaRemoteControl/ctrl").c_str();
-
+void runInteractiveMode(YamahaControl& control)
+{
+    WINDOW * mainwin;
+    if ( (mainwin = initscr()) == NULL ) {
+        fprintf(stderr, "Error initialising ncurses.\n");
+        exit(EXIT_FAILURE);
     }
+    noecho(); // dont' show typed letters
+    keypad (mainwin, TRUE);
+    mvaddstr(0, 0, "commands: o=on/off, up/down/m=volume, n=net_radio, 1-5=hdmi, left/right=station");
+    attron(A_BOLD);
+    mvaddstr(2, 33, "Yamaha Control\n");
+    attroff(A_BOLD);
+    refresh();
 
-    static size_t readHttpResponse(void *ptr, size_t size, size_t count, string *stream);
+    char ch;
+    while((ch = getch()) != 0)
+    {
+        string r;
+        string name;
+        switch(ch) {
+        case 3: name="up"; r=control.run( {"Main_Zone", "Volume", "Lvl"} ,"<Val>Up 2 dB</Val><Exp></Exp><Unit></Unit>", "PUT");  break;
+        case 2: name="down";r=control.run( {"Main_Zone", "Volume", "Lvl"} ,"<Val>Down 2 dB</Val><Exp></Exp><Unit></Unit>", "PUT");  break;
 
-    string runCommand(Cmd& cmd);
-    string runAction(string keyword) {
-        auto iterator= actions.find(keyword);
-        if(iterator!=actions.end()) {
-            pair<string,Cmd> pair = *iterator;
-            Cmd cmd = pair.second;
-            return runCommand(cmd);
+        case 'm': name="mute_on_off";r=control.run( {"Main_Zone", "Volume", "Mute"},"On/Off", "PUT");  break;
+        case '0': name="net_radio_0";;  break;
+        case 'o': name="on_off";r=control.run( "<Main_Zone><Power_Control><Power>On/Standby</Power></Power_Control></Main_Zone>", "PUT");  break;
+        case 'n': name="net_radio";
+            r=control.selectInput("NET");
+            r=control.run( {"NET_RADIO","List_Control","Direct_Sel"} ,"Line_1", "PUT");
+            break;
+        case '1': name="hdmi1";r=control.selectInput("HDMI",1);  break;
+        case '2': name="hdmi2";r=control.selectInput("HDMI",2);  break;
+        case '3': name="hdmi3";r=control.selectInput("HDMI",3);  break;
+        case '4': name="hdmi4";r=control.selectInput("HDMI",4);  break;
+        case '5': name="hdmi5";r=control.selectInput("HDMI",5);  break;
+        }
+
+        if(!name.empty()) {
+            mvprintw(10,33," - %s - ",name.c_str());
+        }
+
+        if(!r.empty()) {
+            mvprintw(14,10,"%s\n", r.c_str());
         } else {
-            return string();
+//            Status status = control.getStatus();
         }
+
+        move(0, 0);
+        refresh();
     }
 
+    /*  Clean up after ourselves  */
 
-    void addAction(string keyword, vector<string> options, string value) {
-        Cmd cmd(options,value);
-        map<string, Cmd>::value_type pair(keyword, cmd);
-        actions.insert(pair);
-    }
-
-    Status getStatus() {
-        Status s;
-        return s;
-    }
-
-    void loadActions() {
-        addAction("down", {"Main_Zone", "Volume", "Lvl"} ,"<Val>Down 2 dB</Val><Exp></Exp><Unit></Unit>");
-        addAction("up",   {"Main_Zone", "Volume", "Lvl"} ,"<Val>Up 2 dB</Val><Exp></Exp><Unit></Unit>");
-        addAction("mute_on_off",   {"Main_Zone", "Volume", "Mute"} ,"On/Off");
-        addAction("net_radio_0",   {"NET_RADIO","List_Control","Direct_Sel"} ,"Line_1");
-        addAction("on_off",   {} ,"<Main_Zone><Power_Control><Power>On/Standby</Power></Power_Control></Main_Zone>");
-        addAction("net_radio",   {"Main_Zone", "Input", "Input_Sel"} ,"NET");
-        addAction("hdmi1",   {"Main_Zone", "Input", "Input_Sel"} ,"HDMI1");
-        addAction("hdmi2",   {"Main_Zone", "Input", "Input_Sel"} ,"HDMI2");
-        addAction("hdmi3",   {"Main_Zone", "Input", "Input_Sel"} ,"HDMI3");
-        addAction("hdmi4",   {"Main_Zone", "Input", "Input_Sel"} ,"HDMI4");
-        addAction("hdmi5",   {"Main_Zone", "Input", "Input_Sel"} ,"HDMI5");
-    }
-
-private:
-    string url;
-    map<string,Cmd> actions;
-};
-
-
-/**
- * @brief YamahaControl::readHttpResponse reads the http response and writes to the string
- */
-size_t YamahaControl::readHttpResponse(void *ptr, size_t size, size_t count, string *stream) {
-    stream->append((char*)ptr, 0, size*count);
-    return size*count;
-}
-
-
-string YamahaControl::runCommand(Cmd& cmd) {
-    string response;
-
-    CURL* curl = curl_easy_init();
-    if(curl) {
-        string xmlString = cmd.toString();
-        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-        curl_easy_setopt(curl, CURLOPT_POST , 1);
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, xmlString.c_str());
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE  , xmlString.length());
-
-        struct curl_slist *slist = curl_slist_append(NULL, "Content-Type: text/xml; charset=utf-8");
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, slist);
-
-        curl_easy_setopt(curl, CURLOPT_TIMEOUT , 1L);
-        curl_easy_setopt(curl, CURLOPT_VERBOSE, 0);
-
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response); // used in readHttpResponse
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &readHttpResponse);
-
-        CURLcode res = curl_easy_perform(curl);
-        if(res != CURLE_OK) {
-            fprintf(stderr, "curl_easy_perform() failed: %s\n",  curl_easy_strerror(res));
-        }
-        curl_slist_free_all(slist);
-        curl_easy_cleanup(curl);
-    }
-    return response;
+    delwin(mainwin);
+    endwin();
+    refresh();
 }
 
 
 int main(int argc, char** argv)
 {
-    if (argc != 3)
+    if (argc != 2)
     {
-        cout << "Usage: " << argv[0] << " <hostname> <action/-i>" << endl;
+        cout << "Usage: " << argv[0] << " <hostname>" << endl;
         cout << "       sets the value on the yamaha device" << endl;
-        cout << "       example: 192.168.1.45 up" << endl;
-        cout << "       use action -i for interactive mode" << endl;
+        cout << "       example: " << argv[0] <<" 192.168.1.45" << endl;
         return -1;
     }
 
     char* hostname = argv[1];
-    string action = argv[2];
 
     YamahaControl control(hostname);
-    control.loadActions();
-
-    // interative
-    if(action.compare("-i")==0) {
-        runInteractiveMode(control);
-    } else {
-        // name
-        string action = std::string(argv[2]);
-        control.runAction(action);
-    }
+    runInteractiveMode(control);
 
     return 0;
 }
